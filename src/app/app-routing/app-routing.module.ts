d813f1265fe74bc7c8d67b17a5c083f279f59a4f
Faisal Khan <faisalkhan91@gmail.com>
2019-12-04 11:59:05 -0500

    Created Routes

--- src/app/app-routing/app-routing.module.ts
+++ src/app/app-routing/app-routing.module.ts
@@ -1,17 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { BooksComponent } from '../books/books.component';
+import { BookEditComponent } from '../books/book-edit/book-edit.component';
+import { BookDetailComponent } from '../books/book-detail/book-detail.component';
 
 const appRoutes: Routes = [
   {path: '', redirectTo: '/books', pathMatch: 'full'},
-  {path: 'books', component: BooksComponent},
+  {path: 'books', component: BooksComponent, children: [
+    {path: '', redirectTo: '/books', pathMatch: 'full'},
+    {path: 'new', component:BookEditComponent},
+    {path: ':id', component: BookDetailComponent},
+    {path: ':id/edit', component:BookEditComponent}
+  ]},
 ]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    //CommonModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class AppRoutingModule { }
