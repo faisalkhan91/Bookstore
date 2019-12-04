ad0f66bf8ab2c11c4b8e968dd0207b31530375bc
Faisal Khan <faisalkhan91@gmail.com>
2019-12-04 10:47:01 -0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BooksComponent } from './books/books.component';
+import { BookListComponent } from './books/book-list/book-list.component';
+import { BookItemComponent } from './books/book-list/book-item/book-item.component';
+import { BookEditComponent } from './books/book-edit/book-edit.component';
+import { BookDetailComponent } from './books/book-detail/book-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksComponent,
+    BookListComponent,
+    BookItemComponent,
+    BookEditComponent,
+    BookDetailComponent
   ],
   imports: [
     BrowserModule,
