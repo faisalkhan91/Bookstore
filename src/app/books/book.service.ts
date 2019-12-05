d4a29c2ab46cbfc09d844768b748bab828bddf78
Faisal Khan <faisalkhan91@gmail.com>
2019-12-05 17:01:36 -0500

    Defined Service Parameters

--- src/app/books/book.service.ts
+++ src/app/books/book.service.ts
@@ -6,27 +6,30 @@ import { Book } from './book';
 })
 export class BookService {
 
-  books: string [] = [];
+  books: Book [] = [
+    new Book("Pyhton for Dummies", "Learn to code python", "url"),
+    new Book("SQL", "Queries for your database", "url"),
+  ];
 
   constructor() { }
 
   AddBook(book: Book)
   {
-
+    this.books.push(book);
   }
 
   DeleteBook(book: Book)
   {
-
+    this.books.pop();
   }
 
   GetBooks()
   {
-
+    return this.books.slice();
   }
 
   GetBook(name: string)
   {
-
-  }
+    return this.books[name];
+  } 
 }
